0f90e3fae52f1bc9983a9535bc8e86ac4d243c75
Ankhanhvu <ank.vu11@gmail.com>
2017-03-06 15:17:47 +0700

    Update gate.py

--- gate.py
+++ gate.py
@@ -3,3 +3,12 @@ class Gate:
         self.x = x
         self.y = y
         self.text = "G "
+
+    def print(self):
+        print(self.x,self.y)
+
+    def match(self,x,y):
+        if self.x == x and self.y == y:
+            return True
+        else:
+            return False
