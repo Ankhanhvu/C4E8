270af7795705711e6dadd2c693bb41de9d45a983
Ankhanhvu <ank.vu11@gmail.com>
2017-03-06 15:16:05 +0700

    Update map.py

--- map.py
+++ map.py
@@ -6,20 +6,20 @@ from gate import Gate
 class Map:
     def __init__(self, width, height):
         self.player = Player(0,0)
-        self.box = Box(3,4)
-        self.gate = Gate(5,5)
+        self.box = Box(1,2)
+        self.gate = Gate(2,1)
         self.width = width
         self.height = height
 
     def print(self):
         for y in range(self.height):
             for x in range (self.width):
-                if self.player.match (x,y):
-                    print(self.player.text, end ="")
-                if self.box.match (x,y):
+                if self.player.match(x,y):
+                    print(self.player.text, end = "")
+                if self.box.match(x,y):
                     print(self.box.text, end = "")
-                if self.gate.match (x,y):
-                    print(self.gate.text, end ="")
+                if self.gate.match(x,y):
+                    print(self.gate.text, end = "")
                 else:
                     print("- ", end = "")
             print()
@@ -36,8 +36,12 @@ class Map:
         self.box.nexty = self.box.y + dy
         if self.in_map(self.player.nextx,self.player.nexty):
             if self.player.match(self.box.x,self.box.y) and self.in_map(self.box.nextx, self.box.nexty):
-                self.player.move_to(self.player.nextx,self.player.nexty)
-                self.box.move_to(self.box.nextx,self.box.nexty)
+                # self.player.move_to(self.player.nextx,self.player.nexty)
+                # self.box.move_to(self.box.nextx,self.box.nexty)
+                self.box.x = self.box.nextx
+                self.box.y = self.box.nexty
+                self.player.x = self.player.nextx
+                self.player.y = self.player.nexty
             elif self.player.match(self.box.x,self.box.y):
                 print("try again")
             else:
@@ -53,7 +57,7 @@ class Map:
         if direction == "w":
             dx, dy = 0, -1
         elif direction == "s":
-            dx,dy = 0, 1
+            dx,dy = 0,1
         elif direction == "a":
             dx,dy = -1, 0
         elif direction == "d":
@@ -63,15 +67,16 @@ class Map:
 
     def loop(self):
         while True:
+            self.print()
             move = input("Your move (w a s d)? ")
             self.process_input(move)
-            self.print()
             if self.box.match(self.gate.x,self.gate.y):
                 print("WIN")
                 break
 
 
-
-
 map = Map(10,10)
+print(map)
 map.loop()
+
+
